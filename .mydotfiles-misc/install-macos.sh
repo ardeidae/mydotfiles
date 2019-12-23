9c79a1d2732f2b73829ddc2ded4c27f74a26c01b
Matthieu Petiot <matthieu.petiot@gmail.com>
2019-12-24 00:03:23 +0100

    Install dockutil and reorganize Dock icons

--- .mydotfiles-misc/install-macos.sh
+++ .mydotfiles-misc/install-macos.sh
@@ -10,3 +10,38 @@ while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
 ./Brewfile
 ./Caskfile
+
+dockutil --no-restart --remove all
+
+dockutil --no-restart --add "/System/Applications/System Preferences.app"
+dockutil --no-restart --add "/System/Applications/App Store.app"
+dockutil --no-restart --add "/System/Applications/Calendar.app"
+dockutil --no-restart --add "/System/Applications/Contacts.app"
+dockutil --no-restart --add "/System/Applications/Mail.app"
+dockutil --no-restart --add "/System/Applications/Music.app"
+
+dockutil --no-restart --add "/Applications/iTerm.app"
+dockutil --no-restart --add "/Applications/MacVim.app"
+dockutil --no-restart --add "/Applications/Sourcetree.app"
+dockutil --no-restart --add "/Applications/Visual Studio Code.app"
+dockutil --no-restart --add "/Applications/Xcode.app"
+dockutil --no-restart --add "/Applications/Insomnia.app"
+dockutil --no-restart --add "/Applications/Postman.app"
+dockutil --no-restart --add "/Applications/VirtualBox.app"
+dockutil --no-restart --add "/Applications/Docker.app"
+dockutil --no-restart --add "/Applications/Kitematic.app"
+
+dockutil --no-restart --add "/Applications/Bitwarden.app"
+dockutil --no-restart --add "/Applications/Chromium.app"
+dockutil --no-restart --add "/Applications/Firefox.app"
+dockutil --no-restart --add "/Applications/Safari.app"
+dockutil --no-restart --add "/Applications/qutebrowser.app"
+dockutil --no-restart --add "/Applications/Dropbox.app"
+dockutil --no-restart --add "/Applications/Microsoft Excel.app"
+dockutil --no-restart --add "/Applications/Microsoft PowerPoint.app"
+dockutil --no-restart --add "/Applications/Microsoft Word.app"
+dockutil --no-restart --add "/Applications/Signal.app"
+dockutil --no-restart --add "/Applications/Transmission.app"
+dockutil --no-restart --add "/Applications/VLC.app"
+
+killall Dock
