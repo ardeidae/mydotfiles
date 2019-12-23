332fc89586513339965a7dbb01214806ec99e017
Matthieu Petiot <matthieu.petiot@gmail.com>
2019-12-24 00:02:20 +0100

    Start Brewfile and Caskfile scripts

--- .mydotfiles-misc/install-macos.sh
+++ .mydotfiles-misc/install-macos.sh
@@ -7,3 +7,6 @@ sudo -v
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+./Brewfile
+./Caskfile
