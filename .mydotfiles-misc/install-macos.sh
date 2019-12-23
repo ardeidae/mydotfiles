8ee049237b54d3fdc6d583161065f1896916f409
Matthieu Petiot <matthieu.petiot@gmail.com>
2019-12-24 00:00:23 +0100

    Install brew.sh

---
+++ .mydotfiles-misc/install-macos.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# ask for the administrator password upfront
+sudo -v
+
+# keep-alive: update existing `sudo` time stamp until this script has finished
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
